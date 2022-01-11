e65c74d789efff0ec6a752498088fbfb42a729cf
Lee Jae Hyun <myleje@gmail.com>
2022-01-11 14:18:41 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -1,3 +1,5 @@
+#pragma warning disable IDE0052, CS0108
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +9,13 @@ public class FireCtrl : MonoBehaviour
     public Transform firePos; // 총알을 생성할 위치
     public GameObject bulletPrefab; // Bullet 프리팹을 저장할 변수
     public AudioClip fireSfx; // 총 발사 사운드 파일을 저장할 변수
+
+    private AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
